3f3c06feba1b9d17a0f4e680bb803c17b00b28f5
sankaungkin <sankaungkin@gmail.com>
2024-05-20 08:14:14 +0630

    category crud

--- internal/handler/categoryHandler.go
+++ internal/handler/categoryHandler.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gofiber/fiber"
 	"github.com/sankangkin/di-rest-api/internal/models"
 	"github.com/sankangkin/di-rest-api/internal/service"
+	"gorm.io/gorm"
 )
 
 type categoryHandler struct {
@@ -39,4 +42,91 @@ func(h *categoryHandler) CreateCategoryHandler(c *fiber.Ctx) error{
 			"message": "category has been created successfully",
 			"data":    newCategory,
 		})
+}
+
+func(h *categoryHandler) GetAllCategoriesHandler(c *fiber.Ctx) error{
+	categories, err := h.srv.GetCategories() 
+	if err != nil {
+		return  c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+	return c.Status(http.StatusOK).JSON(
+		&fiber.Map{
+			"status":  "SUCCESS",
+			"message": len(categories),
+			"data":    categories,
+		})
+}
+
+func(h *categoryHandler) GetCategoryById(c *fiber.Ctx) error {
+	id, err := strconv.ParseUint(c.Params("id"), 10,32)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	category, err := h.srv.GetCategoryById(uint(id))
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"status":  "FAIL",
+				"message": "Record not found",
+			})
+		}
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"status": "FAIL", "message": err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  "SUCCESS",
+		"message": "Record found",
+		"data":    category,
+	})
+}
+
+func(h *categoryHandler) UpdateCatagory(c *fiber.Ctx) error {
+	id, err := strconv.ParseUint(c.Params("id"), 10,32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	category, err := h.srv.GetCategoryById(uint(id))
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"status":  "FAIL",
+				"message": "Record not found",
+			})
+		}
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"status": "FAIL", "message": err.Error(),
+		})
+	}
+	h.srv.UpdateCategory(category)
+	return c.JSON(fiber.Map{
+		"code":    200,
+		"message": "Update successfully",
+	})
+	
+}
+
+func(h *categoryHandler) DeleteCategory(c *fiber.Ctx) error {
+	id, err := strconv.ParseUint(c.Params("id"), 10,32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	category, err := h.srv.GetCategoryById(uint(id))
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"status":  "FAIL",
+				"message": "Record not found",
+			})
+		}
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"status": "FAIL", "message": err.Error(),
+		})
+	}
+	h.srv.DeleteCategory(uint(category.ID))
+	return c.JSON(fiber.Map{
+		"code":    200,
+		"message": "Delete successfully",
+	})
 }
