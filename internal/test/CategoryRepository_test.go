8096b52d175157e0e626e4b40cffd6f43927d546
sankaungkin Oo <sankaungkin@gmail.com>
2024-08-29 19:33:57 +0630

    category test completed

--- internal/test/CategoryRepository_test.go
+++ internal/test/CategoryRepository_test.go
@@ -5,141 +5,161 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/sankangkin/di-rest-api/internal/domain/category"
+	c "github.com/sankangkin/di-rest-api/internal/domain/category"
 	"github.com/sankangkin/di-rest-api/internal/models"
-
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
-
 	p "gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var categories = []models.Category{
-	{
-		CategoryName: "Construction Materials",
-	},
-	{
-		CategoryName: "Sanitary Ware",
-	},
-	{
-		CategoryName: "PVC Pipe",
-	},
-	{
-		CategoryName: "PVC Fitting",
-	},
-	{
-		CategoryName: "GI Fitting",
-	},
+type CategoryRepositoryTestSuite struct {
+	suite.Suite
+	db        *gorm.DB
+	repo      c.CategoryRepositoryInterface
+	container testcontainers.Container
 }
 
-func SetUpDBContainer(ctx context.Context) (testcontainers.Container, *gorm.DB, error) {
-	dbName := "testPosdb"
-	dbUser := "user"
-	dbPassword := "passw0rd"
+func TestCategoryRepositoryTestSuite(t *testing.T) {
+	suite.Run(t, &CategoryRepositoryTestSuite{})
+}
 
-	req := testcontainers.ContainerRequest(testcontainers.ContainerRequest{
+func (suite *CategoryRepositoryTestSuite) SetupSuite() {
+
+	suite.T().Log("---------SetupSuite()--------")
+	// Set up the Testcontainers PostgreSQL container
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
 		Image:        "postgres:13-alpine",
 		ExposedPorts: []string{"5432/tcp"},
 		Env: map[string]string{
-			"POSTGRES_USER":     dbUser,
-			"POSTGRES_PASSWORD": dbPassword,
-			"POSTGRES_DB":       dbName,
+			"POSTGRES_USER":     "testuser",
+			"POSTGRES_PASSWORD": "testpassword",
+			"POSTGRES_DB":       "testdb",
 		},
-
 		WaitingFor: wait.ForListeningPort("5432/tcp"),
-	})
-	pgContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+	}
+	postgresContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: req,
 		Started:          true,
 	})
+	suite.NoError(err)
+	suite.container = postgresContainer
 
-	if err != nil {
-		return nil, nil, err
+	// Get the container's IP address and port
+	host, err := postgresContainer.Host(ctx)
+	suite.NoError(err)
+	port, err := postgresContainer.MappedPort(ctx, "5432")
+	suite.NoError(err)
+
+	// Create the database connection string
+	dsn := fmt.Sprintf("host=%s port=%s user=testuser password=testpassword dbname=testdb sslmode=disable", host, port.Port())
+
+	// Connect to the database using GORM
+	db, err := gorm.Open(p.Open(dsn), &gorm.Config{})
+	suite.NoError(err)
+	suite.NotNil(db) // Ensure db is not nil
+
+	// Auto migrate the Category model
+	err = db.AutoMigrate(&models.Category{})
+	suite.NoError(err)
+
+	// Assign the db and repo to the suite
+	suite.db = db
+	suite.repo = c.NewCategoryRepository(db)
+	suite.NotNil(suite.repo) // Ensure repo is not nil
+}
+
+func (suite *CategoryRepositoryTestSuite) TearDownSuite() {
+	suite.T().Log("----------TearDownSuite()----------")
+
+	err := suite.container.Terminate(context.Background())
+	suite.NoError(err)
+}
+
+func (suite *CategoryRepositoryTestSuite) TestGetAll() {
+
+	suite.T().Log("----------TestGetAll()----------")
+
+	for _, category := range MockCategories {
+		err := suite.db.Create(&category).Error
+		suite.NoError(err)
 	}
 
-	port, err := pgContainer.MappedPort(ctx, "5432")
-	if err != nil {
-		return nil, nil, err
+	categories, err := suite.repo.GetAll()
+	suite.NoError(err)
+	suite.NotNil(categories)
+	suite.Equal(6, len(categories))
+
+	suite.T().Log(categories)
+	// Check if "Category 2" is present in the list
+	found := false
+	for _, category := range MockCategories {
+		if category.CategoryName == "Construction Materials" {
+			found = true
+			break
+		}
 	}
-
-	connURL := fmt.Sprintf("host=localhost port=%s user=%s password=%s dbname=%s sslmode=disable", port.Port(), dbUser, dbPassword, dbName)
-	db, err := gorm.Open(p.Open(connURL), &gorm.Config{
-		// Logger: logger.Default.LogMode(logger.Info),
-	})
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// fmt.Println("Seeding categories data ....")
-	// db.Create(&categories)
-
-	return pgContainer, db, nil
+	suite.True(found, `"Construction Materials" should be present in the categories list`)
 
 }
 
-func TestCategoryRepo_Create(t *testing.T) {
-	ctx := context.Background()
-	container, gormDB, err := SetUpDBContainer(ctx)
-	if err != nil {
-		t.Fatalf("could not start container: %v", err)
-	}
-	assert.NoError(t, err)
+func (suite *CategoryRepositoryTestSuite) TestCreate() {
 
-	defer func() {
-		err := container.Terminate(ctx)
-		if err != nil {
-			t.Fatalf("failed to terminate container: %v", err)
-		}
-	}()
-
-	err = gormDB.AutoMigrate(&models.Category{})
-	assert.NoError(t, err)
-	catRepo := category.NewCategoryRepository(gormDB)
+	suite.T().Log("-----------TestCreate()---------")
 
 	newCategory := &models.Category{
 		CategoryName: "Test Category",
 	}
 
-	assert.NoError(t, err)
-	assert.NotNil(t, newCategory)
+	suite.NotNil(newCategory)
 
-	assert.Equal(t, "Test Category", newCategory.CategoryName)
+	suite.Equal("Test Category", newCategory.CategoryName)
 
-	if _, err := catRepo.Create(newCategory); err != nil {
-		t.Fatalf("failed to create category: %v", err)
+	if _, err := suite.repo.Create(newCategory); err != nil {
+		suite.T().Fatalf("failed to create category: %v", err)
 	}
-	assert.NoError(t, err)
-	t.Log("new category has been created", newCategory)
+
+	// suite.T().Log("new category has been created", newCategory)
 	var count int64
-	err = gormDB.Model(&models.Category{}).Where("category_name = ?", "Test Category").Count(&count).Error
-	assert.NoError(t, err)
-	assert.Equal(t, int64(1), count, "Category should be created in the database")
+	err := suite.db.Model(&models.Category{}).Where("category_name = ?", "Test Category").Count(&count).Error
+	suite.NoError(err)
+	suite.Equal(int64(1), count, "Category should be created in the database")
+}
 
-	for _, category := range categories {
-		err = gormDB.Create(&category).Error
-		assert.NoError(t, err)
-	}
+func (suite *CategoryRepositoryTestSuite) TestGetById() {
+	suite.T().Log("------------TestGetById()-------------")
 
-	categories, err := catRepo.GetAll()
-	assert.NoError(t, err)
-	assert.NotNil(t, categories)
-	assert.Equal(t, 6, len(categories))
-	assert.Equal(t, "Test Category", categories[0].CategoryName)
-	assert.Equal(t, "Construction Materials", categories[1].CategoryName)
+	category, err := suite.repo.GetById(5)
+	suite.NoError(err)
+	suite.NotNil(category)
+	suite.Equal("PVC Fitting", category.CategoryName)
+}
 
-	// Check if "Category 2" is present in the list
-	found := false
-	for _, category := range categories {
-		if category.CategoryName == "Test Category" {
-			found = true
-			break
-		}
-	}
-	assert.True(t, found, `"Construction Materials" should be present in the categories list`)
+func (suite *CategoryRepositoryTestSuite) TestUpdate() {
+	suite.T().Log("------------TestUpdate()--------------")
 
-	t.Cleanup(func() {
-		gormDB.Migrator().DropTable(&models.Category{})
-	})
+	category, err := suite.repo.GetById(5)
+	suite.NoError(err)
+	suite.NotNil(category)
+
+	category.CategoryName = "Updated Category"
+	updatedCategory, err := suite.repo.Update(category)
+	suite.NoError(err)
+	suite.NotNil(updatedCategory)
+	suite.Equal("Updated Category", updatedCategory.CategoryName)
+}
+
+func (suite *CategoryRepositoryTestSuite) TestZDelete() {
+	suite.T().Log("--------------TestDelete()------------")
+
+	// delete the category with ID 2
+	err := suite.repo.Delete(2)
+	suite.NoError(err)
+
+	//check category with ID 2 was still exited or not
+	category, err := suite.repo.GetById(2)
+	suite.Error(err)
+	suite.Nil(category)
 }
---
+++ internal/test/mockCategories.go
@@ -0,0 +1,21 @@
+package test
+
+import "github.com/sankangkin/di-rest-api/internal/models"
+
+var MockCategories = []models.Category{
+	{
+		CategoryName: "Construction Materials",
+	},
+	{
+		CategoryName: "Sanitary Ware",
+	},
+	{
+		CategoryName: "PVC Pipe",
+	},
+	{
+		CategoryName: "PVC Fitting",
+	},
+	{
+		CategoryName: "GI Fitting",
+	},
+}
